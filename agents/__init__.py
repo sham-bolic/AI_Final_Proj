230a81b39e425ce10f4c4fe632bd45c7f7d7a11c
sham-bolic <143747815+sham-bolic@users.noreply.github.com>
2023-11-19 00:44:48 -0500

    creating random agents

--- agents/__init__.py
+++ agents/__init__.py
@@ -3,3 +3,5 @@ from .random_agent import RandomAgent
 from .human_agent import HumanAgent
 from .student_agent import StudentAgent
 from .random2_agent import RandomAgent2
+from .random3_agent import Random3Agent
+from .random4_agent import random4Agent
